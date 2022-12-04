1dd3a0c5b3614773bfaf78898a8150802966ef9d
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2022-12-04 18:24:35 +0300

    написала html и js

---
+++ index.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.inputName');
+const btnReady = document.querySelector('.btnReady');
+// const result = document.querySelector()
+btnReady.addEventListener('click', fullName);
+
+function fullName {
+    let text = input.value;
+    let textTrimmed = text.trim();
+    let arr = textTrimmed.split(" ");
+    let lastName = arr[0];
+    let firstName = arr[1];
+    let patronimic = arr[2];
+    let lastNameFixed = lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
+    let firstNameFixed = firstName[0].toUpperCase() + firstName.slice(1).toLowerCase();
+    let paronimicFixed = patronimic[0].toUpperCase() + patronimic.slice(1).toLowerCase();
+    document.querySelector('#lastName').innerHTML = 'Фамилия: ' + lastNameFixed;
+    document.querySelector('#firstName').innerHTML = 'Имя: ' + firstNameFixed;
+    document.querySelector('#patronimic').innerHTML = 'Отчество: ' + paronimicFixed;
+}
+// не работает, и я не понимаю, почему
+
